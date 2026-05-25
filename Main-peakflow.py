4a5b82da8fdf5607c35891d5439d067602dacb3f
vidujay2006-boop <vidujay2006@gmail.com>
2026-05-25 18:10:41 +0100

    Refactor gender and age functions; add height input
    Refactor gender and age input functions to separate male age handling. Added height input function for user.

--- Main-peakflow.py
+++ Main-peakflow.py
@@ -5,10 +5,10 @@ def askGender():
     gen=input("Enter your gender ('m'/'f'): ")
     if gen == 'm'.lower():
         print("You are a male")
-        askAge()
+        askAgeMale()
     elif gen == 'f'.lower(): 
         print("You are female")
-        askAge()
+        askAgeMale()
 
     else: 
         print("Enter a valid gender (m or f) ")
@@ -16,16 +16,23 @@ def askGender():
 
 
 
-def askAge():
+def askAgeMale():
     try:
         age = int(input("Please enter your age: "))
         if age <= 0:
             print("Enter a valid age")
-            askAge()
+            askAgeMale()
         else:
             print("Test complete")
     except ValueError:
         print("Error: enter a number only")
-        askAge()
+        askAgeMale()
 
 askGender()
+
+def askHeight(): 
+    option=int(input("Enter your height, do you choose to enter in inches (1) or cm (2): ")
+               if option == 1: 
+                   height_inches=("Eneter your height in inches: ")
+                   final_hight = height_inches * 2.54
+                   
